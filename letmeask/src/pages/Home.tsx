9cf9bc92c32b5051a911f0ef0fefa2fe45488bb1
Ferreira-Mateus <mateus.andrade@bandtec.com.br>
2021-06-22 23:23:22 -0300

    NLW Aula 3

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase'
+import { auth, database, firebase } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -10,13 +10,15 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss'
-import { useContext } from 'react';
+import { FormEvent, useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { useState } from 'react';
 
 export function Home() {
 
   const history = useHistory();
   const { user, signInWithGoogle } = useContext(AuthContext);
+  const [roomCode, setRoomCode] = useState('');
   
 
   async function handleCreateRoom() {
@@ -26,6 +28,25 @@ export function Home() {
     }
     
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.')
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+    
+
   }
 
   return (
@@ -47,10 +68,12 @@ export function Home() {
           <div className="separator">
             ou entre em uma sala
           </div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
